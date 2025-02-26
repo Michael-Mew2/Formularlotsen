21424c69096b8a968b705a98e6d0cad704132fc9
dci-student <mmn.umfragen@gmail.com>
2025-02-26 17:51:40 +0100

    build hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,24 +13,14 @@ export default function Hero() {
       <div className="hero">
         <HeroPicture />
         <div className="hero hero--overlay">
-          <h1
-            style={{
-              backgroundColor: "green",
-              margin: "20px",
-              display: "flex",
-            }}
-          >
-            {texts[currentPage]?.hero?.heroMainTitle || "lädt..."}
-          </h1>
-          <h2
-            style={{
-              backgroundColor: "green",
-              margin: "20px",
-              display: "flex",
-            }}
-          >
-            {texts[currentPage]?.hero?.heroSubTitle || "lädt..."}
-          </h2>
+          <div className="hero--insidebox">
+            <h1 className="hero--title">
+              {texts[currentPage]?.hero?.heroMainTitle || "lädt..."}
+            </h1>
+            <h2 className="hero--subtitle">
+              {texts[currentPage]?.hero?.heroSubTitle || "lädt..."}
+            </h2>
+          </div>
         </div>
       </div>
     </>
