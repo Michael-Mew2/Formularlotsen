512b26ff15e44ab7d9fc74d6c336085e83e1d12e
dci-student <mmn.umfragen@gmail.com>
2025-03-04 17:52:18 +0100

    finish Homepage

--- src/components/FooterCredits.jsx
+++ src/components/FooterCredits.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
 
-export default function FooterCredits() {
+export default function FooterCredits({ footerTexts }) {
   return (
-    <div>FooterCredits</div>
-  )
+    <div className="footerCredits">
+      <div className="upperBlock">
+        <p>{footerTexts.footer?.credits?.recreation?.text}</p>
+      </div>
+      <div className="specialThanks">
+        <p>{footerTexts.footer?.credits?.original?.text}</p>
+      </div>
+    </div>
+  );
 }
--- src/components/FooterExternal.jsx
+++ src/components/FooterExternal.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import * as React from 'react'
 
-export default function FooterExternal() {
+export default function FooterExternal({footerTexts}) {
+
   return (
-    <div>FooterExternal</div>
-  )
+    <div className='footerExternal'>
+      {footerTexts.footer?.externalLinks.map((content, index) => (
+        <p key={index}>
+          {content.text}{' '}
+          {content.links?.map((link, linkIndex) => (
+            <React.Fragment key={linkIndex}>
+              <a href={link.url} target="_blank" rel="noopener noreferrer">{link.name}</a>
+              {linkIndex < content.links.length - 1 ? ' & ' : ''}
+            </React.Fragment>
+          ))}
+        </p>
+      ))}
+    </div>
+  );
 }
 
