18be880830362f596da4e9c34acbbbf2b3087e99
Michael-Mew2 <michael.mdnev@gmail.com>
2025-05-20 15:20:21 +0200

    [feature]: edit header and nav scss
    made the header and the nav-bar smaller, so that there is more space for the page-content

--- src/components/BremerhavenMap.jsx
+++ src/components/BremerhavenMap.jsx
@@ -21,6 +21,40 @@ import { useLanguageStore } from "../store";
 // ==========
 
 export default function BremerhavenMap() {
+  const [locationData, setLocationData] = React.useState(null);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const { language } = useLanguageStore();
+
+  React.useEffect(() => {
+    const loadLocationInfo = async () => {
+      try {
+        setIsLoading(true);
+        const response = await fetch(
+          `texte/locales/components/locationData/${language}.json`
+        );
+        if (!response.ok) throw new Error(`HTTP-Fehler: ${response.status}`);
+
+        const data = await response.json();
+        console.log("Daten erfolgreich geladen:", data);
+
+        setLocationData(data.locationData.data.locations);
+      } catch (error) {
+        console.error(`Fehler beim laden der Datei: ${error}`);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    loadLocationInfo();
+  }, [language]);
+
+  React.useEffect(() => {
+    if (locationData === null) {
+      console.log("locationData noch null");
+    } else {
+      console.log("LocationData:", locationData);
+    }
+  }, [locationData]);
   // demo-center:
   const demoCenter = [];
 
@@ -51,34 +85,38 @@ export default function BremerhavenMap() {
     return new divIcon({
       html: `<div class="cluster-icon">${cluster.getChildCount()}</div>`,
       className: "custom-marker-cluster",
-      iconSize: point(33, 33, true)
-    })
-  }
+      iconSize: point(33, 33, true),
+    });
+  };
 
   // ----------
   return (
     <div className="bremerhaven-map-container" style={{ height: "600px" }}>
-      <MapContainer
-        center={[53.5395845, 8.5729424]} // Zentrum der Stadt
-        zoom={11}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-
-        <MarkerClusterGroup
-          chunkedLoading // Erlaubt es React alle Marker einzeln zu laden anstatt alle zusammen
-          iconCreateFunction={createCustomClusterIcon}
-
+      {isLoading ? (
+        <div className="map-loading">Map is loading...</div>
+      ) : (
+        <MapContainer
+          center={[53.5395845, 8.5729424]} // Zentrum der Stadt
+          zoom={11}
         >
-          {demoMarkers.map((marker) => (
-            <Marker position={marker.geocode} icon={customIcon}>
-              <Popup>{marker.popUp}</Popup>
-            </Marker>
-          ))}
-        </MarkerClusterGroup>
-      </MapContainer>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+
+
+            {locationData?.map((marker, index) => (
+              <Marker
+                key={`marker-${index}`}
+                position={[marker.pinPosition.lat, marker.pinPosition.lng]}
+                icon={customIcon}
+              >
+                <Popup>{marker.location}</Popup>
+              </Marker>
+            ))}
+
+        </MapContainer>
+      )}
     </div>
   );
 }
