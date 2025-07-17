b88978165da0e66d3496e0d829e7ff054fcc2d74
Michael-Mew2 <michael.mdnev@gmail.com>
2025-07-17 12:31:57 +0200

    create TimeTable.jsx

--- src/components/TimeTable.jsx
+++ src/components/TimeTable.jsx
@@ -5,6 +5,20 @@ export default function TimeTable({position}) {
     const [loading, setIsLoading] = React.useState(true);
     const {language} = useLanguageStore();
 
+    React.useEffect(() => {
+        const loadTimeInfo = async () => {
+            try {
+                setIsLoading(true);
+                const response = await fetch(
+                    `texte/locales/components/locationData/${language}.json`
+                );
+                
+            } catch (error) {
+                
+            }
+        }
+    })
+
   return (
     <div className={`${position}`}>
         <p>Das ist ein kleiner Text</p>
