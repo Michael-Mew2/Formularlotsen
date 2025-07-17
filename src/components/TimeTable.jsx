a1e287a8d4c32f5894fc182c480f7f6df170ff0a
Michael-Mew2 <michael.mdnev@gmail.com>
2025-07-17 13:06:26 +0200

    fetch timeData @ TimeTable.jsx

--- src/components/TimeTable.jsx
+++ src/components/TimeTable.jsx
@@ -3,6 +3,7 @@ import { useLanguageStore } from '../store';
 
 export default function TimeTable({position}) {
     const [loading, setIsLoading] = React.useState(true);
+    const [timeData, setTimeData] = React.useState(null);
     const {language} = useLanguageStore();
 
     React.useEffect(() => {
@@ -12,12 +13,23 @@ export default function TimeTable({position}) {
                 const response = await fetch(
                     `texte/locales/components/locationData/${language}.json`
                 );
-                
+                if(!response.ok) throw new Error(`HTTP-Fehler: ${response.status}`);
+
+                const data = await response.json();
+                console.log("Daten erfolgreich geladen:", data);
+                setTimeData(data.locationData)
+                setIsLoading(false);
             } catch (error) {
-                
+                console.error("Fehler beim Laden der Daten", error);
+                setIsLoading(false);
             }
-        }
-    })
+        };
+
+        loadTimeInfo();
+    }, [language]);
+
+    // Sortieren der Standorte nach Tag und Uhrzeit
+    
 
   return (
     <div className={`${position}`}>
