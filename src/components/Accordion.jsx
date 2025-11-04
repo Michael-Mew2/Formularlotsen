68a354af44acfec7025cdf526fd5261fa9873204
Michael-Mew2 <michael.mdnev@gmail.com>
2025-11-04 18:26:37 +0100

    [feature]: add outer styling

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import PageSection from "./PageSection";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const FlexGroup = ({ direction = "row", flex, children, className = "" }) => {
   return (
@@ -26,52 +27,71 @@ const FlexItem = ({ flex, children, className = "" }) => {
 };
 
 export default function Accordion({ section }) {
-    console.log("Accordion section:", section);
-    
+  const [openIndex, setOpenIndex] = React.useState(-1);
+
+  const toggleAccordion = (index) =>
+    setOpenIndex(index === openIndex ? -1 : index);
+
+  console.log("Accordion section:", section);
+
   return (
     <div className={`accordion ${section.groupStyle || ""}`}>
       {section.items?.map((item, index) => (
-        <div key={index} className="accordion-item">
-          {item.title && <h3 className="accordion-title">{item.title}</h3>}
-          <div className="accordion-content">
-            {item.contentGroups?.map((group, groupIdx) => (
-              <FlexGroup
-                key={groupIdx}
-                direction={group.direction}
-                flex={group.flex}
-                className={group.className}
-              >
-                {group.items?.map((groupItem, itemIdx) =>
-                  groupItem.direction ? (
-                    <FlexGroup
-                      key={itemIdx}
-                      direction={groupItem.direction}
-                      flex={groupItem.flex}
-                      className={groupItem.className}
-                    >
-                      {groupItem.items?.map((nestedItem, nestedIdx) => (
-                        <FlexItem
-                          key={nestedIdx}
-                          flex={nestedItem.flex}
-                          className={nestedItem.className}
-                        >
-                          <PageSection section={nestedItem.component} />
-                        </FlexItem>
-                      ))}
-                    </FlexGroup>
-                  ) : (
-                    <FlexItem
-                      key={itemIdx}
-                      flex={groupItem.flex}
-                      className={groupItem.className}
-                    >
-                      <PageSection section={groupItem.component} />
-                    </FlexItem>
-                  )
-                )}
-              </FlexGroup>
-            ))}
-          </div>
+        <div
+          key={index}
+          className={`accordion-item ${openIndex === index ? "open" : ""}`}
+        >
+          {item.title && (
+            <div
+              className={`accordion-title ${openIndex === index ? "open" : ""}`}
+              onClick={() => toggleAccordion(index)}
+              style={{ cursor: "pointer" }}
+            >
+              <h3>{item.title}</h3>
+              <FontAwesomeIcon className={`accordion-icon ${openIndex === index ? "open" : ""}`} icon="fa-solid fa-play" />
+            </div>
+          )}
+          {/* {openIndex === index && ( */}
+            <div className="accordion-content">
+              {item.contentGroups?.map((group, groupIdx) => (
+                <FlexGroup
+                  key={groupIdx}
+                  direction={group.direction}
+                  flex={group.flex}
+                  className={group.className}
+                >
+                  {group.items?.map((groupItem, itemIdx) =>
+                    groupItem.direction ? (
+                      <FlexGroup
+                        key={itemIdx}
+                        direction={groupItem.direction}
+                        flex={groupItem.flex}
+                        className={groupItem.className}
+                      >
+                        {groupItem.items?.map((nestedItem, nestedIdx) => (
+                          <FlexItem
+                            key={nestedIdx}
+                            flex={nestedItem.flex}
+                            className={nestedItem.className}
+                          >
+                            <PageSection section={nestedItem.component} />
+                          </FlexItem>
+                        ))}
+                      </FlexGroup>
+                    ) : (
+                      <FlexItem
+                        key={itemIdx}
+                        flex={groupItem.flex}
+                        className={groupItem.className}
+                      >
+                        <PageSection section={groupItem.component} />
+                      </FlexItem>
+                    )
+                  )}
+                </FlexGroup>
+              ))}
+            </div>
+          {/* )} */}
         </div>
       ))}
     </div>
