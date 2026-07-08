b896fc4593a04597d3f986fde64f529f22b68dde
Michael-Mew2 <michael.mdnev@gmail.com>
2026-07-08 11:08:23 +0200

    [⚙ feature]: create tina/components.js

---
+++ tina/schema/collections/componens.js
@@ -0,0 +1,15 @@
+export const componentCollection = {
+  name: "components",
+  label: "Komponenten",
+  path: "public/texte/locales/components",
+  format: "json",
+
+  ui: {
+    allowedActions: {
+      create: false,
+      delete: false,
+    },
+  },
+
+  fields: [],
+};
